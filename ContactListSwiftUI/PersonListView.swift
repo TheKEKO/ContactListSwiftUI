f9ba9c0ec5baf83c09d4d815c9dd166a88b5c57e
Aleksandr F <a7214370@gmail.com>
2022-05-28 23:35:50 +0300

    Setup PersonListView

---
+++ ContactListSwiftUI/PersonListView.swift
@@ -0,0 +1,20 @@
+//
+//  PersonListView.swift
+//  ContactListSwiftUI
+//
+//  Created by Aleksandr F. on 28.05.2022.
+//
+
+import SwiftUI
+
+struct PersonListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PersonListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonListView()
+    }
+}
