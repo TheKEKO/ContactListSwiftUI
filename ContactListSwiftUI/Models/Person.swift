e5116f8a298c77f6549242695e248fcbcde69ed0
Aleksandr F <a7214370@gmail.com>
2022-05-29 03:13:57 +0300

    Work with Person

--- ContactListSwiftUI/Models/Person.swift
+++ ContactListSwiftUI/Models/Person.swift
@@ -5,4 +5,61 @@
 //  Created by Aleksandr F. on 28.05.2022.
 //
 
-import Foundation
+
+import SwiftUI
+
+struct Person: Identifiable {
+    
+    let id = UUID()
+    
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
+
+extension Person {
+    static func getContactList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataManager.shared.names.shuffled()
+        let surnames = DataManager.shared.surnames.shuffled()
+        let emails = DataManager.shared.emails.shuffled()
+        let phones = DataManager.shared.phones.shuffled()
+        
+        let iterationCount = min(
+            names.count,
+            surnames.count,
+            emails.count,
+            phones.count
+        )
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                surname: surnames[index],
+                email: emails[index],
+                phoneNumber: phones[index]
+            )
+
+            persons.append(person)
+        }
+        return persons
+    }
+}
+
+extension Color {
+    static func random() -> Color {
+        return Color (
+        red: .random(in: 0..<1),
+        green: .random(in: 0..<1),
+        blue: .random(in: 0..<1)
+        )
+    }
+}
+
