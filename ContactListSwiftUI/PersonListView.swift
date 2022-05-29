aea949bff57049347517226e563f4aa916943ea1
Aleksandr F <a7214370@gmail.com>
2022-05-29 03:15:12 +0300

    Work with PersonListView

--- ContactListSwiftUI/PersonListView.swift
+++ ContactListSwiftUI/PersonListView.swift
@@ -5,16 +5,33 @@
 //  Created by Aleksandr F. on 28.05.2022.
 //
 
+
 import SwiftUI
 
 struct PersonListView: View {
+    let persons: [Person]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(persons) { person in
+            NavigationLink(destination: PersonDetailedView(person: person)) {
+                ZStack {
+                    Circle()
+                        .fill(Color.random())
+                        .frame(width: 50, height: 50)
+                    Text(person.fullName.prefix(1))
+                }
+                Text(person.fullName)
+            }
+        }
+        .listStyle(.grouped)
     }
 }
 
 struct PersonListView_Previews: PreviewProvider {
     static var previews: some View {
-        PersonListView()
+        PersonListView(persons: Person.getContactList())
     }
 }
+
+
+
