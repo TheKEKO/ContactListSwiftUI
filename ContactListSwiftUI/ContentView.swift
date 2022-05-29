3eec517cc60a3b9145a7b9d0540a4cdb2f467322
Aleksandr F <a7214370@gmail.com>
2022-05-29 03:16:06 +0300

    Work with ContentView

--- ContactListSwiftUI/ContentView.swift
+++ ContactListSwiftUI/ContentView.swift
@@ -5,12 +5,39 @@
 //  Created by Aleksandr F. on 28.05.2022.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
+    let persons = Person.getContactList()
+    @State private var search = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            TabView {
+                PersonListView(persons: searchResults)
+                    .tabItem {
+                        Image(systemName: "person.2.fill")
+                        Text("Contacts")
+                    }
+                PersonListDetailsView(persons: searchResults)
+                    .tabItem{
+                        Image(systemName: "phone.fill")
+                        Text("Numbers")
+                    }
+            }
+            .navigationTitle("Contact List")
+            .searchable(text: $search, prompt: "Enter your First or Last Name")
+        }
+        .colorScheme(.dark)
+    }
+    
+    private var searchResults: [Person] {
+        if search.isEmpty {
+            return persons
+        } else {
+            return persons.filter { $0.fullName.contains(search) }
+        }
     }
 }
 
